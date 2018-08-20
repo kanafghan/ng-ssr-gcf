e22eb274d03a0cf7d1179314d371645e8be1217e
Ismail Faizi <kanafghan@gmail.com>
2018-08-20 15:16:37 +0200

    add angularfire2 together with firebase JS SDK

--- pwa/src/app/app.server.module.ts
+++ pwa/src/app/app.server.module.ts
@@ -4,7 +4,6 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   imports: [
     AppModule,
